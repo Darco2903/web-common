47dbd769b061eb41483de0caf0520fe1706c6d7a
Darco2903 <114527594+Darco2903@users.noreply.github.com>
2026-03-01 14:09:21 +0100

    fix: internal isMobile  typo

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,14 @@
 let hostname = "";
 let isLocalhost = false;
 let hasSubdomain = false;
-let iSMobile = false;
+let isMobile = false;
 
 if (typeof window !== "undefined") {
     hostname = new URL(document.baseURI).hostname;
     isLocalhost = ["localhost", "127.0.0.1"].includes(hostname); // Check if localhost
     hasSubdomain = (hostname.match(/\./g) || []).length > 1;
-    iSMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent);
+    isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent);
 }
 
 export const DOMAIN = !isLocalhost && hasSubdomain ? hostname.replace(/^[^.]+\./g, "") : hostname; // Remove subdomain
-export const IS_MOBILE = iSMobile;
+export const IS_MOBILE = isMobile;
